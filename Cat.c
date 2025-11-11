28875b48bcf96b6d296d53b9a14c69a35180e76f
zbuthorne <zbuthorne@huskers.unl.edu>
2025-11-11 12:50:33 -0600

    Added cat names

---
+++ Cat.c
@@ -0,0 +1,14 @@
+/**
+*Author: Zoey Buthorne
+*
+*Date: 11/11/2025
+*
+*Prints out cat names
+*/
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    printf("Leo\n");
+    printf("Pheobe\n");
+    return 0;
+}
