a4d43fe0bc2c10d6852ae1765e77e585bc0d3567
zbuthorne <zbuthorne@huskers.unl.edu>
2025-11-12 17:03:23 +0000

    Wed, Nov 12, 2025, 11:03 AM -06:00

--- Week3/dataUsage.c
+++ Week3/dataUsage.c
@@ -23,9 +23,15 @@
     double dataLeft = numberGB - data Usage;
     double avgUse = dataUsage / dayOfMonth;
     double goodUse = numberGB / 30;
+    double useOver = (avgUse * daysLeft) - numberGB;
+    double newAvg = (dataLeft / daysLeft);
     if (avgUse > goodUse) {
         printf("%f used, %f remaining\n", dayOfMonth, daysLeft);
         printf("Average Daily Use: %f gb/day\n", avgUse);
-        printf("You are exceeding your correct Avg daily use (%f)
-    }
+        printf("You are exceeding your correct Avg daily use (%f)\n", goodUse);
+        printf("Continuing this high usage you will exceed your data plan by\n");
+        printf("%f GB\n", useOver);
+        printf("To stay below your data plan use no more than %f GB/day\n", newAvg);
+
+    } else if (avgUse )
  }
