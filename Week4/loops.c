de384dcd15570fd5f1bd75ab9594caa09d9c93f0
zbuthorne <zbuthorne@huskers.unl.edu>
2025-11-25 01:09:16 +0000

    Mon, Nov 24, 2025, 7:09 PM -06:00

--- Week4/loops.c
+++ Week4/loops.c
@@ -11,7 +11,12 @@
 
  int main(int argc, char argv) {
     int n = 10;
+    int count = 0;
     for(int i=0; i <n; i++0) {
-        for(int j=0;)
-    }
+        for(int j=0; j<n; j++) {
+            printf("i, j = %d, %d\n" , i , j);
+            count ++;
+        }
+
+        printf
  }
